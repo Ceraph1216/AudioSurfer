d0f8b415bc09d73661cc1d248ebe1198a7fa23b9
Drew Nicolo <MaverickHunterDN@gmail.com>
2017-01-21 10:52:23 -0500

    - added UI sprites to project - added input options for all 3 tricks - added display for total score

--- Audio Surfer Project/Assets/Scripts/Managers/ScoreManager.cs
+++ Audio Surfer Project/Assets/Scripts/Managers/ScoreManager.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour 
 {
 	public static ScoreManager instance;
 
-	public int currentScore;
+	public Text scoreText;
 	public float currentComboScore;
+
+	private int _currentScore;
+	public int currentScore
+	{
+		get 
+		{
+			return _currentScore;
+		}
+		set 
+		{
+			_currentScore = value;
+			scoreText.text = _currentScore.ToString ();
+		}
+	}
 
 	public int[] starThreshholds;
 
