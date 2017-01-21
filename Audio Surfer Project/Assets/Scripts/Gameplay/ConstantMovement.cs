3f232d16f71d28cf93538f441546f4d9e5f092c0
Drew Nicolo <MaverickHunterDN@gmail.com>
2017-01-20 22:59:22 -0500

    - added test scene with line renderer that follows mid-range on song - added Code Monkey as test song

---
+++ Audio Surfer Project/Assets/Scripts/Gameplay/ConstantMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMovement : MonoBehaviour 
+{
+	public Vector3 moveSpeed;
+
+	private Transform _transform;
+
+	void Awake ()
+	{
+		_transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		_transform.Translate (moveSpeed * Time.deltaTime);
+	}
+}
