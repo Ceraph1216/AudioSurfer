40cd6a63c757f4a0f95212d421c011f85245c5ff
Drew Nicolo <MaverickHunterDN@gmail.com>
2017-01-21 22:43:25 -0500

    - removed compression from idle animation - added title screen - renamed song used

---
+++ Audio Surfer Project/Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour 
+{
+	public void OnClick ()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("GameScreen");
+	}
+
+	void Update ()
+	{
+		if (Input.GetAxis ("Submit") > 0) 
+		{
+			OnClick ();
+		}
+	}
+}
