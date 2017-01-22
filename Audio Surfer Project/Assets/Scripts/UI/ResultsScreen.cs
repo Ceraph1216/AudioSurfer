96fb7fd74a07d379f7af2ff0dfcddb01aa51058a
Drew Nicolo <MaverickHunterDN@gmail.com>
2017-01-21 23:34:50 -0500

    - added win/lose message to results screen - added play again button to results screen

---
+++ Audio Surfer Project/Assets/Scripts/UI/ResultsScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultsScreen : MonoBehaviour 
+{
+	public Text resultsText;
+
+	void OnEnable ()
+	{
+		if (ScoreManager.instance.currentScore > ScoreManager.instance.starThreshholds [2]) 
+		{
+			resultsText.text = "Congratulations! You win!";
+		} 
+		else 
+		{
+			resultsText.text = "Oh no! You lost!";
+		}
+	}
+
+	public void OnPlayClick ()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("GameScreen");
+	}
+}
