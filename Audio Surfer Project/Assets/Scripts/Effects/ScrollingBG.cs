87d645a74b4d3c848bb95c18942abee7251af798
Drew Nicolo <MaverickHunterDN@gmail.com>
2017-01-22 10:50:27 -0500

    - added parallaxing backgrounds

---
+++ Audio Surfer Project/Assets/Scripts/Effects/ScrollingBG.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingBG : MonoBehaviour 
+{
+	public Camera BGcamera;
+	public float moveSpeed;
+	public float width;
+
+	private Transform _transform;
+
+	void Awake ()
+	{
+		_transform = transform;
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Move ();
+		CheckWrap ();
+	}
+
+	private void Move ()
+	{
+		Vector3 l_newPosition = _transform.position;
+		l_newPosition.x += moveSpeed * Time.deltaTime;
+		_transform.position = l_newPosition;
+	}
+
+	private void CheckWrap ()
+	{
+		Vector3 l_screenPosition = BGcamera.WorldToScreenPoint (_transform.position);
+		if (l_screenPosition.x < 0) 
+		{
+			Vector3 l_newPosition = _transform.position;
+			l_newPosition.x += width;
+			_transform.position = l_newPosition;
+		}
+	}
+}
